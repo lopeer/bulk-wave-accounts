539f622080844b4bdbb31d3df5db80ac2fa29e7f
Eze Sunday Eze <mailsforeze@gmail.com>
2021-10-20 14:39:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+const libCrypto = require('@waves/ts-lib-crypto')
+const fs = require('fs');
+const numberOfAccounts = process.argv[2];
+
+if (!numberOfAccounts) {
+    return console.log('Please enter number of accounts to generate as the first argument')
+}
+
+const generateAddresses = (numberOfAccounts) => {
+    try {
+        const seed = libCrypto.randomSeed() // or input your existing seed
+        const sk = libCrypto.privateKey(seed)
+        const pk = libCrypto.publicKey(seed)
+        const addressBase58 = libCrypto.address(seed) // address for Mainnet
+        const addressTestnetBase58 = libCrypto.address(seed, 'T') // address for Testnet
+
+        const account = {
+            passPhrase: seed,
+            address: addressBase58,
+            privateKey: sk,
+            publicKey: pk,
+            testnetAddress: addressTestnetBase58
+        }
+
+        let accounts = []
+
+        for (let i=0; i<=numberOfAccounts; i++){
+            accounts.push(account)
+        }
+
+        let data = JSON.stringify(accounts);
+        fs.writeFileSync('accounts.json', data);
+        console.log(`${numberOfAccounts} Accounts generated successfully`);
+
+    } catch(error) {
+        return  error
+    }
+}
+
+generateAddresses(numberOfAccounts)
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Bulk Wave Blockchain account generator
+
+## Installation
+
+```bash
+npm install -g bulkwave
+```
+
+## Usage
+
+```bash
+bulkwave <numberOfAddresses>
+```
+
+e.g bulkwave 5000
+
+This will generate 500 wave blockchain accounts with Private keys, public, keys, testnet address, mainnet addresses, and passphrase
